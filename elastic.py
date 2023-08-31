f6bf49edecef761fe4ad2ac6149663360ff58934
HernanRodriguez1 <66162160+HernanRodriguez1@users.noreply.github.com>
2023-08-31 16:32:55 -0500

    Update elastic.py

--- elastic.py
+++ elastic.py
@@ -4,33 +4,19 @@ import requests
 def main(host):
     port = 9200
 
-    # Construir la URL del endpoint _cat/indices?v
     indices_url = f"http://{host}:{port}/_cat/indices?v"
-
-    # Realizar la solicitud GET para obtener los índices disponibles
     response = requests.get(indices_url)
 
-    # Verificar el estado de la respuesta
     if response.status_code == 200:
         print("Índices disponibles:")
         print(response.text)
 
-        # Solicitar al usuario que ingrese un índice
         index = input("\nIngresa el nombre del índice al que deseas acceder: ")
-
-        # Construir la URL del endpoint _search?pretty= para el índice seleccionado
         search_url = f"http://{host}:{port}/{index}/_search?pretty="
-
-        # Solicitar la consulta de búsqueda al usuario
         search_query = input("Ingresa la consulta de búsqueda: ")
-
-        # Construir la URL completa para la búsqueda utilizando GET
         full_search_url = f"{search_url}{search_query}"
-
-        # Realizar la solicitud GET para buscar en el índice seleccionado
         search_response = requests.get(full_search_url)
 
-        # Verificar el estado de la respuesta
         if search_response.status_code == 200:
             print("Resultados de la búsqueda:")
             print(search_response.text)
